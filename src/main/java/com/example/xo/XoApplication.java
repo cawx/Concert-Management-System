8cf6721977f6fa55381542011c1ba2b9e1aea023
Caroly <93351229+cawx@users.noreply.github.com>
2024-08-01 22:46:31 +0300

    first commit

---
+++ src/main/java/com/example/xo/XoApplication.java
@@ -0,0 +1,13 @@
+package com.example.xo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XoApplication.class, args);
+	}
+
+}
